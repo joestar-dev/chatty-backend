b9dc84c6a25863a66ef5150eb598a5a3971bbf0d
Joestar <kandit_pkdev@hotmail.com>
2024-02-08 09:51:54 +0700

    feat: added reaction feature

---
+++ src/shared/sockets/chat.ts
@@ -0,0 +1,20 @@
+import { ISenderReceiver } from '@chat/interfaces/chat.interface';
+import { Socket, Server } from 'socket.io';
+
+export let socketIOChatObject: Server;
+
+export class SocketIOChatHandler {
+  private io: Server;
+  constructor(io: Server) {
+    this.io = io;
+    socketIOChatObject = io;
+  }
+
+  public listen(): void {
+    this.io.on('connection', (socket: Socket) => {
+      socket.on('join room', (data: ISenderReceiver) => {
+        console.log(data);
+      });
+    })
+  }
+}
